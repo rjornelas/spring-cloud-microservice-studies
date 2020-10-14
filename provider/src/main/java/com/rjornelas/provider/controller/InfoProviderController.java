c44da31668e706faa9da7d03e57339017f26e507
rjornelas <rafael.j.ornelas@gmail.com>
2020-10-13 23:34:40 -0300

    Alteracao yml, dependencias e controller

--- provider/src/main/java/com/rjornelas/provider/controller/InfoProviderController.java
+++ provider/src/main/java/com/rjornelas/provider/controller/InfoProviderController.java
@@ -3,20 +3,21 @@ package com.rjornelas.provider.controller;
 import com.rjornelas.provider.entity.InfoProvider;
 import com.rjornelas.provider.service.InfoProviderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/providers")
+@RequestMapping("/info")
 public class InfoProviderController {
 
     @Autowired
     private InfoProviderService infoProviderService;
 
-    @RequestMapping("/{estado}")
-    public InfoProvider getInfoProvidersByState(@PathVariable String estado){
-        return infoProviderService.getInfoProvidersByState(estado);
+    @GetMapping("/{state}")
+    public InfoProvider getInfoProvidersByState(@PathVariable String state){
+        return infoProviderService.getInfoProvidersByState(state);
     }
 
 }
