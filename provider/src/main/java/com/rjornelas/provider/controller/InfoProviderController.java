1d62ccbb3c2657185f5ccdcc642ab3bfb0c3eb00
Igor José Guimarães <idd_iguimaraes@uolinc.com>
2020-11-10 17:21:09 -0300

    ordem prioridade propriedades no bootstrap

--- provider/src/main/java/com/rjornelas/provider/controller/InfoProviderController.java
+++ provider/src/main/java/com/rjornelas/provider/controller/InfoProviderController.java
@@ -3,10 +3,13 @@ package com.rjornelas.provider.controller;
 import com.rjornelas.provider.model.InfoProvider;
 import com.rjornelas.provider.service.InfoProviderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 @RequestMapping("/info")
@@ -15,9 +18,22 @@ public class InfoProviderController {
     @Autowired
     private InfoProviderService infoProviderService;
 
+    @Value("${configVar}")
+    private String configVar;
+
+    @Value("${spring.application.name}")
+    private String appName;
+
     @GetMapping("/{state}")
     public InfoProvider getInfoProvidersByState(@PathVariable String state){
         return infoProviderService.getInfoProvidersByState(state);
+    }
+    
+    @GetMapping
+    public String getConfig() {
+        var msgConfigVar = String.format("Config Variable: %s", configVar);
+        var msgAppName = String.format("Application Name: %s", appName);
+        return String.format("%s <br> %s", msgAppName, msgConfigVar);
     }
 
 }
